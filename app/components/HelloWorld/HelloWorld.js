d0994c8c86267556130323b6b257f37be1a84bd3
Ian Golden <ian@iangolden.com>
2017-03-07 14:27:51 -0700

    Working test-suite with babel-root-import

--- app/components/HelloWorld/HelloWorld.js
+++ app/components/HelloWorld/HelloWorld.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export class HelloWorld extends Component {
     render() {
       return(
-        <h1>Nah</h1>
+        <h1>Hello, world.</h1>
       )
     }
 }
--- app/containers/index.js
+++ app/containers/index.js
@@ -1,2 +1,2 @@
-export {AppContainer} from './App/AppContainer'
+export { AppContainer } from './App/AppContainer'
 
